4463fe7b184a0ecda86f201f648f76469be98568
doniataskman <doniahammud@gmail.com>
2020-12-13 13:00:54 +0200

    new 13.12.2020

--- app/src/main/java/awad/ali/aliproject2020/Data/MyTaskAdapter.java
+++ app/src/main/java/awad/ali/aliproject2020/Data/MyTaskAdapter.java
@@ -1,12 +1,17 @@
 package awad.ali.aliproject2020.Data;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import awad.ali.aliproject2020.R;
 
 public class MyTaskAdapter extends ArrayAdapter<MyTask> {
     public MyTaskAdapter(@NonNull Context context, int resource) {
@@ -15,7 +20,58 @@ public class MyTaskAdapter extends ArrayAdapter<MyTask> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View v = LayoutInflater.from(getContext()).inflate(R.layout.activity_my_task, parent, false);
+        TextView tvTitle = v.findViewById(R.id.itmTvTitle);
+        TextView tvImportant = v.findViewById(R.id.itmTvImportant);
+        TextView tvNecessary = v.findViewById(R.id.itmTvNecessary);
+        TextView tvSupject = v.findViewById(R.id.itmTvSubject);
+        ImageButton btnDelete = v.findViewById(R.id.itmbtnDelete);
+        ImageButton btnCall = v.findViewById(R.id.itmbtnCall);
+        ImageButton btnEdit = v.findViewById(R.id.itmbtnEdit);
+        MyTask task = getItem(position);
+        tvTitle.setText(task.getTitle());
+        tvSupject.setText(task.getSub());
+        tvNecessary.setText(task.getNec());
+        tvImportant.setText(task.getImportant);
+        switch (task.getImportant) {
+            case 5:
+                tvImportant.setBackgroundColor(Color.RED);
+                break;
+            case 4:
+                tvImportant.setBackgroundColor(Color.YELLOW);
+                break;
+            case 3:
+                tvImportant.setBackgroundColor(Color.CYAN);
+                break;
+            case 2:
+                tvImportant.setBackgroundColor(Color.MAGENTA);
+                break;
+            case 1:
+                tvImportant.setBackgroundColor(Color.rgb(0, 200, 0));
+                break;
+        }
+        switch (task.getNec()) {
+            case 5:
+                tvNecessary.setBackgroundColor(Color.RED);
+                break;
+            case 4:
+                tvNecessary.setBackgroundColor(Color.YELLOW);
+                break;
+            case 3:
+                tvNecessary.setBackgroundColor(Color.CYAN);
+                break;
+            case 2:
+                tvNecessary.setBackgroundColor(Color.MAGENTA);
+                break;
+            case 1:
+                tvNecessary.setBackgroundColor(Color.rgb(0, 200, 0));
+                break;
+
+
+            return super.getView(position, convertView, parent);
+        }
     }
 }
+
--- app/src/main/java/awad/ali/aliproject2020/MyUi/MainActivity.java
+++ app/src/main/java/awad/ali/aliproject2020/MyUi/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import awad.ali.aliproject2020.R;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton ibtnAdd;
+    ListView lst
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
